e7618f6e3b5cc0b8a47be414464fde0b983ccac8
jose raphael <jose.bu@ccc.ufcg.edu.br>
2021-04-21 10:51:01 -0300

    finalização do curso da udemy

---
+++ api-gateway/api-gateway/src/test/java/com/estudo/v2/microservices/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.v2.microservices.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
